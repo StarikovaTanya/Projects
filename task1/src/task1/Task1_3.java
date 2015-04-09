78022395ecb3c942714eb5f09a802102a9aa00e4
StarikovaTanya <bliznycshky1995@mail.ru>
2015-04-09 22:10:53 +0300

    mlkl

---
+++ task1/src/task1/Task1_3.java
@@ -0,0 +1,13 @@
+package task1;
+import java.util.Random;
+import java.util.Scanner;
+public class Task1_3 {
+	public static void main(String[] args){
+		int i;
+		Random j= new Random();
+		for(i=1; i<=10;i++){//kol-vo shtuk
+		//	System.out.print(j.nextInt(10)+" ");
+			System.out.println(j.nextInt(20));//promegytok
+		}
+	}}
+
